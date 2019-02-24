891e1066cc02b1a37fc95cb42da61b1f02f686b7
Bogus Franczak <bfranczak@gmail.com>
2019-02-24 12:23:37 +0100

    Complete task

--- App.js
+++ App.js
@@ -9,25 +9,37 @@ var Counter = React.createClass({
             counter: this.state.counter + 1
         });
     },
-    dicrement: function () {
+    decrement: function () {
         this.setState({
             counter: this.state.counter - 1
         })
     },
+    componentDidMount: function() {
+        console.log('e.g. assign class or event handler');
+    },
+    componentWillUnmount: function() {
+        console.log('e.g. remove event handler or class');
+    },
     render: function () {
-        return React.createElement('div', {},
-            React.createElement('span', {}, 'Licznik ' + this.state.counter),
-            React.createElement('button', { onClick: this.increment }, 'Increment (+)'),
-            React.createElement('button', { onClick: this.dicrement }, 'Dicrement (-)')
-        );
+        if(isEven(this.props.id)) {
+            return React.createElement('div', {id: this.props.id},
+                React.createElement('span', {}, 'Licznik ' + this.state.counter),
+                React.createElement('button', { onClick: this.increment }, 'Increment (+)'),
+                React.createElement('button', { onClick: this.decrement }, 'Decrement (-)')
+            );
+        } else return null
     }
 });
 
-var counters = [1, 2, 3, 4, 5, 6];
+var counters = [1, 2, 3, 4, 5, 6, 7, 8];
+var counterElements = counters.map(function(el) {
+    return React.createElement(Counter, {key: el, id: el})
+});
 
-var element = React.createElement('div', {}, counters.map(function (el) {
-    return React.createElement(Counter, { key: el });
-})
-);
+var element = React.createElement('div', {}, counterElements);
+
 ReactDOM.render(element, document.getElementById('app'));
 
+function isEven(num) {
+    return num % 2 === 0
+}
